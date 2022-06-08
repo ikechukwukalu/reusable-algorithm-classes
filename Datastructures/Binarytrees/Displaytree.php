0d0e2199b7f0bf9113a4391f47db8bc9288f8d87
ikechukwukalu <ikaykaly@gmail.com>
2022-06-09 00:34:09 +0100

    Implementing Binary Search Tree

--- Datastructures/Binarytrees/Displaytree.php
+++ Datastructures/Binarytrees/Displaytree.php
@@ -9,35 +9,6 @@ class Displaytree extends BST {
 
     }
 
-    public function displayListRightMovement(BST $tree): void
-    {
-        $tree->itemIndex = $tree->item = null;
-
-        $i = 0;
-        $count = $tree->size();
-
-        while ($i < $count) {
-            echo 'VALUE: ' . $tree->getCurrentItem()->value . ', INDEX: ' . $tree->getCurrentItemIndex() . '<br/>';
-            $tree->nextItem();
-            $i ++;
-        }
-
-        echo '<br/>';
-    }
-
-    public function displayListLeftMovement(BST $tree): void
-    {
-        $j = $tree->size() - 1;
-
-        while ($j >= 0) {
-            echo 'VALUE: ' . $tree->getCurrentItem()->value . ', INDEX: ' . $tree->getCurrentItemIndex() . '<br/>';
-            $tree->prevItem();
-            $j --;
-        }
-
-        echo '<br/>';
-    }
-
     public function displayUsingPreTag($object): void
     {
         echo '<pre>';
