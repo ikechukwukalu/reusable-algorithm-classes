a2450c146c4307a6d4b38ee6d6fab625f080e05e
ikechukwukalu <ikaykaly@gmail.com>
2022-06-24 13:39:23 +0100

    Updated Binary Search Tree, JS

--- Datastructures/Js/Binarytrees/BST.js
+++ Datastructures/Js/Binarytrees/BST.js
@@ -1,3 +1,5 @@
+"use strict";
+exports.__esModule = true;
 var NodeFunc = function (value) {
     return {
         value: value,
@@ -119,24 +121,4 @@ var BinaryTreeFunc = function (firstValue) {
         breadthFirstSort: breadthFirstSort
     };
 };
-var tree = BinaryTreeFunc(15);
-tree.insert(3);
-tree.insert(36);
-tree.insert(2);
-tree.insert(12);
-tree.insert(28);
-tree.insert(39);
-process.stdout.write('\nInitial Tree In Object Format\n');
-console.log(tree);
-process.stdout.write('\nShow The Min Branch In Object Format\n');
-console.log(tree.min());
-process.stdout.write('\nShow The Max Branch In Object Format\n');
-console.log(tree.max());
-process.stdout.write('\nDepth First Sort In Order\n');
-console.log(tree.depthFirstSortInOrder());
-process.stdout.write('\nDepth First Sort Pre-order\n');
-console.log(tree.depthFirstSortPreOrder());
-process.stdout.write('\nDepth First Sort Post-order\n');
-console.log(tree.depthFirstSortPostOrder());
-process.stdout.write('\Breadth First Sort\n');
-console.log(tree.breadthFirstSort());
+exports["default"] = BinaryTreeFunc;
